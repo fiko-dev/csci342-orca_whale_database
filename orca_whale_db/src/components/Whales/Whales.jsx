bf0871e893d6d02fdf1fc90b58477ae17674302f
Liam <rockl@wwu.edu>
2024-02-06 13:18:21 -0800

    Added components folder

---
+++ orca_whale_db/src/components/Whales/Whales.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Whales = () => {
+    return (
+        <div>
+            <h1>Whales of the Pacific Northwest!</h1>
+
+        </div>
+    );
+};
+
+export default Whales;

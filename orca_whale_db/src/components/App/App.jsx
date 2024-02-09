f54cd946fca2a79c1680ded503a1af05344bd464
melnikd <melnikd@wwu.edu>
2024-02-09 01:29:52 -0800

    started on home page

--- orca_whale_db/src/components/App/App.jsx
+++ orca_whale_db/src/components/App/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Whales from './components/Whales';
+import { useState } from 'react';
+import './App.css';
+import HomePage from '../HomePage/main/HomePage';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Whales />
+      <HomePage/>
     </>
   )
 }
---
+++ orca_whale_db/src/components/HomePage/main/HomePage.jsx
@@ -0,0 +1,17 @@
+
+import Banner from "../Banner/Banner"
+import Card from "../Card/Card"
+import Navbar from "../NavBar/NavBar"
+
+function HomePage() {
+  
+    return (
+      <>
+        <Navbar/>
+        <Banner/>
+        <Card/>
+      </>
+    )
+  }
+  
+  export default HomePage

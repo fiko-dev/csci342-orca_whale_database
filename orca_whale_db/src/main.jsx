626952cad3ed0b47b4750e43459585b629e2fddb
Liam <rockl@wwu.edu>
2024-02-20 12:59:32 -0800

    Extra

--- orca_whale_db/src/main.jsx
+++ orca_whale_db/src/main.jsx
@@ -7,12 +7,14 @@ import Whales from './components/Whales/Whales'
 import NotFoundPage from './components/NotFound/NotFoundPage'
 import HomePage from './components/HomePage/main/HomePage'
 import AboutUs from './components/AboutUs/AboutUs'
+import OrcaBio from './components/WhaleBios/OrcaBio/OrcaBio'
 
 
 const router = createBrowserRouter([
   {path:"/",element:<App />, children:[ // Replace App with hompage
     {index:true, element: <HomePage />},
     {path:"whales", element: <Whales />},
+    {path:"whales/orcas", element: <OrcaBio />},
     {path:"aboutus", element: <AboutUs />},
     {path:"*", element: <NotFoundPage />}
   ]}

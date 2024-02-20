b3be89094d0269af9f6a74e6bd78df1e4b69153e
jedchristian-pagcaliwagan <pagcalj@wwu.edu>
2024-02-20 13:04:49 -0800

    Removed Card component from HomePage.

--- orca_whale_db/src/components/HomePage/main/HomePage.jsx
+++ orca_whale_db/src/components/HomePage/main/HomePage.jsx
@@ -1,4 +1,5 @@
 import CreateDiscussion from "../../CreateDiscussion/CreateDiscussion"
+import MapComponent from "../../MapComponent/MapComponent"
 import Sightings from "../../Sightings/Sightings"
 import Banner from "../Banner/Banner"
 import Card from "../Card/Card"
@@ -9,7 +10,6 @@ function HomePage() {
         <Banner/>
         <Sightings />
         <CreateDiscussion/>
-        <Card/>
       </>
     )
   }

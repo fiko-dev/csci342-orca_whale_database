52be3b6975178d3e21a70ad3d5cba6b93ad1bb65
jedchristian-pagcaliwagan <pagcalj@wwu.edu>
2024-02-20 13:36:30 -0800

    Removed unnecessary imports.

--- orca_whale_db/src/components/HomePage/main/HomePage.jsx
+++ orca_whale_db/src/components/HomePage/main/HomePage.jsx
@@ -1,8 +1,6 @@
 import CreateDiscussion from "../../CreateDiscussion/CreateDiscussion"
-import MapComponent from "../../MapComponent/MapComponent"
 import Sightings from "../../Sightings/Sightings"
 import Banner from "../Banner/Banner"
-import Card from "../Card/Card"
 
 function HomePage() {
     return (

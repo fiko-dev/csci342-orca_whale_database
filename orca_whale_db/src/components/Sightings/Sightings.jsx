c8d7dcb402a0516579933ac24114f7f77feeb4d0
Chris Farrer <chrisotopherrfarrer@gmail.com>
2024-02-28 15:08:37 -0800

    update

--- orca_whale_db/src/components/Sightings/Sightings.jsx
+++ orca_whale_db/src/components/Sightings/Sightings.jsx
@@ -1,12 +1,27 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 // import OrcaInfo from "../OrcaInfo/OrcaInfo";
 import MapComponent from "../MapComponent/MapComponent";
 
 const Sightings = () => {
+  const [sightings, setSightings] = useState([]);
+
+  useEffect(() => {
+    const fetchSightings = async () => {
+      const res = await fetch(
+        import.meta.env.VITE_SERVER_URL + "/sightings"
+      ).catch((err) => {
+        console.log("Error: " + err);
+      });
+      const data = await res.json();
+      setSightings(data);
+    };
+    fetchSightings();
+  }, []);
+
   return (
     <div className="Sightings p-[20px] content-center items-center justify-center text-center">
       <h1 className="font-bold mb-5">Reported Sightings!</h1>
-      <MapComponent />
+      <MapComponent sightings={sightings} />
       <br />
     </div>
   );

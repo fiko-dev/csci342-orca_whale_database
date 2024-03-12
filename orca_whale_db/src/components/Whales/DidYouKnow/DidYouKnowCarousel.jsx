07cfe36840ba7e5f12e50d396c60f889c22a284d
Ustr1221 <khamtsang@gmail.com>
2024-03-11 20:01:35 -0700

    added animations

--- orca_whale_db/src/components/Whales/DidYouKnow/DidYouKnowCarousel.jsx
+++ orca_whale_db/src/components/Whales/DidYouKnow/DidYouKnowCarousel.jsx
@@ -6,22 +6,25 @@ import DidYouKnow from "./DidYouKnow"
 
 function DidYouKnowCarousel ({ data }) {
     const [slide, setSlide] = useState(0);
+    const [direction, setDirection] = useState(true); 
 
     const nextSlide = () => {
         setSlide(slide === data.length - 1 ? 0 : slide + 1);
+        setDirection(true);
     }
 
     const prevSlide = () => {
         setSlide(slide === 0 ? data.length - 1: slide - 1);
+        setDirection(false);
     }
 
     return (
-        <div className="dyk-carousel">
-            <BsArrowLeftCircleFill className="arrow arrow-left" onClick={prevSlide} />
+        <div className="dyk-carousel overflow-hidden">
+            <BsArrowLeftCircleFill className="arrow arrow-left z-[2]" onClick={prevSlide} />
             {data.map((item, idx) => {
-                return <DidYouKnow fact={item.fact} img={item.img} key={idx} className={slide === idx ? "dyk-slide" : "slide-hidden"} />
+                return <DidYouKnow fact={item.fact} img={item.img} key={idx} className={slide === idx ? ( direction===true ? "dyk-slide-right": "dyk-slide-left") : "slide-hidden"} />
             })}
-            <BsArrowRightCircleFill className="arrow arrow-right" onClick={nextSlide} />
+            <BsArrowRightCircleFill className="arrow arrow-right  z-[2]" onClick={nextSlide} />
             <span className="indicators">
                 {data.map((_, idx) => {
                     return <button key={idx} onClick={() => setSlide(idx)} className={slide === idx ? "indicator" : "indicator indicator-inactive"}></button>

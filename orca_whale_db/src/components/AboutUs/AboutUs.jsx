d692374f0042a7146215bbc9fd23dbaa2018da8d
Ustr1221 <khamtsang@gmail.com>
2024-03-15 22:45:16 -0700

    "converted css to tailwind in AboutUs, Account, App, and DiscussionPost pages"

--- orca_whale_db/src/components/AboutUs/AboutUs.jsx
+++ orca_whale_db/src/components/AboutUs/AboutUs.jsx
@@ -1,14 +1,14 @@
 import { Link } from 'react-router-dom';
 import Banner from '../HomePage/Banner/Banner';
-import './AboutUs.css';
+
 
 const AboutUs = () => {
     return (
         <div>
             <Banner title={"About Us"} backgroundImage={"./src/assets/day-tours-banner.jpg"}/>
-            <div className='content'>
-                <h1>Who are we? What is our mission?</h1>
-                <p className='text-black text-center'>
+            <div className='relative flex flex-col items-center justify-center h-[80vh]'>
+                <h1 className='pt-5 text-center'>Who are we? What is our mission?</h1>
+                <p className='text-black text-center p-5'>
                 We are students at Western Washington Univerity and we noticed that within the whale-watchong community, there lacks a central place where people with simlar interests 
                 can interact with one another and discuss about sighting while also learning about the biology of the whales they are sight seeing. Our team has decided to create a such an application.
                 Our project , the Orca/Whale Database App, is an interactive and social database for those interested in following information about the Southern Resident orcas 
